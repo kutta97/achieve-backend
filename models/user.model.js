674e214ee4a4d5ec8588321040a38576513a719f
kutta97 <yhjin04170@gmail.com>
2022-11-04 05:09:59 +0900

    user 모델 추가

---
+++ models/user.model.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        unique: true,
+      },
+      name: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      profileImageURL: {
+        type: Sequelize.STRING(200),
+        allowNull: true,
+      },
+      password: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+    }, {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+    });
+  }
+
+  static associate(db) {
+    this.belongsToMany(db.User, {
+      foreignKey: 'userId',
+      as: 'friends',
+      through: 'UsersFriends',
+    });
+    this.belongsToMany(db.User, {
+      foreignKey: 'friendId',
+      as: 'userFriends',
+      through: 'UsersFriends',
+    });
+  }
+};
