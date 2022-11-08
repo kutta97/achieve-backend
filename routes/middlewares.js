4ecb24c043ca43596244ffa675eedacb6277a6fa
kutta97 <yhjin04170@gmail.com>
2022-11-08 13:31:13 +0900

    verifyToken 추가

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     return res.status(403).json({'ok' : false, 'message' : 'Login needed'});
@@ -11,3 +13,23 @@ exports.isNotLoggedIn = (req, res, next) => {
   }
   next();
 };
+
+exports.verifyToken = (req, res, next) => {
+  console.log('decoded', req.headers);
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        ok: false,
+        message: 'Token Expired'
+      })
+    } else {
+      return res.status(401).json({
+        ok: false,
+        message: 'Invalid Token'
+      })
+    }
+  }
+}
