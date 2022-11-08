77d5437d17ddc59c0881d7db653d9f759bcdc26a
kutta97 <yhjin04170@gmail.com>
2022-11-08 09:33:14 +0900

    server.js passport 관련 코드 추가

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
+const path = require('path')
+const passport = require('passport')
+const session = require('express-session');
 const dotenv = require('dotenv');
 const cors = require("cors");
 
 dotenv.config();
+
+const authRouter = require('./routes/auth.routes');
 const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
+passportConfig();
 
 const corsOptions = {
-  origin: "http://localhost:8080"
+  origin: "http://localhost:3000"
 };
 
 app.set('port', process.env.PORT || 8080);
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
   .then(() => {
     console.log('database connected');
   })
@@ -25,11 +28,37 @@ sequelize.sync({ force: true })
     console.error(err);
   });
 
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRouter);
+
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} router not found.`);
   error.status = 404;
   next(error);
 });
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Server is running on port ${app.get('port')}.`);
