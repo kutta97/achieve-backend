d1e768e6c6d5c7395c900e78cdb3ef8c594261e4
kutta97 <yhjin04170@gmail.com>
2022-11-08 13:30:32 +0900

    로그인 시 jwt 토근 전송하는 기능 추가

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,51 +1,60 @@
 const express = require('express');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user.model');
 
 const router = express.Router();
 
-router.post('/signup', isNotLoggedIn, async (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
   const { email, name, password } = req.body;
   try {
     const exUser = await User.findOne({ where: { email } });
     if (exUser) {
       return res.json({'ok' : false, 'message' : 'Already exist'})
+    } else {
+      const hash = await bcrypt.hash(password, 12);
+      await User.create({
+        email,
+        name,
+        password: hash,
+      });
+      return res.json({'ok' : true, 'message' : 'Sign up success'})
     }
-    const hash = await bcrypt.hash(password, 12);
-    await User.create({
-      email,
-      name,
-      password: hash,
-    });
-    return res.json({'ok' : true, 'message' : 'Sign up success'})
-
   } catch (error) {
     console.error(error);
     return next(error);
   }
 });
 
-router.post('/login', isNotLoggedIn, (req, res, next) => {
+router.post('/login', (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
     if (authError) {
       return res.json(authError);
     }
-    if (!user) {
+    else if (!user) {
       return res.json({'ok' : false, 'message' : info.message});
+    } else {
+      return req.login(user, (loginError) => {
+        if (loginError) {
+          console.error(loginError);
+          return next(loginError);
+        }
+        const token = jwt.sign({
+          id: user.id,
+          name: user.name
+        }, process.env.JWT_SECRET, {
+          expiresIn: '1h',
+          issuer: 'achieve',
+        })
+        return res.json({'ok' : true, 'message' : 'Login success', 'data' : { accessToken : token }});
+      });
     }
-    return req.login(user, (loginError) => {
-      if (loginError) {
-        console.error(loginError);
-        return next(loginError);
-      }
-      return res.json({'ok' : true, 'message' : 'Login success'});
-    });
   })(req, res, next);
 });
 
-router.get('/logout', isLoggedIn, (req, res) => {
+router.get('/logout', (req, res) => {
   req.logout();
   req.session.destroy();
 });
