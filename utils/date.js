3a4a96fd4750f0a97b749d50cf5462b99cb979b9
kutta97 <yhjin04170@gmail.com>
2022-12-18 01:24:19 +0900

    date util 추가

---
+++ utils/date.js
@@ -0,0 +1,24 @@
+var exports = module.exports = {};
+
+exports.getDateString = (date) => {
+  const dateObject = new Date(date);
+  const y = dateObject.getFullYear();
+  const m = (dateObject.getMonth() + 1).toLocaleString(undefined, {minimumIntegerDigits: 2});
+  const d = dateObject.getDate().toLocaleString(undefined, {minimumIntegerDigits: 2});
+  
+  return `${y}.${m}.${d}`
+};
+
+exports.getDday = (due) => {
+  const today = new Date();
+  const dueDate = new Date(due);
+
+  if (exports.getDateString(today) === exports.getDateString(dueDate)) {
+    return '-day'
+  }
+
+  const diff = dueDate - today;
+  const dday = Math.floor(diff / (1000*60*60*24))
+
+  return (diff < 0) ? `+${-dday}` : `-${dday}`;
+}
